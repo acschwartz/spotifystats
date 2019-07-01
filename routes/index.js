d1fd1c7b2247041b9e9d2a014735c46098d87d84
Anna Schwartz <schw1781@umn.edu>
2019-06-30 23:16:23 -0500

    skeleton for artists

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var artists_short_term = require('../data/artists_short_term.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', artists: artists_short_term.items });
 });
 
 module.exports = router;
